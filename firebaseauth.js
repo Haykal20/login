4f8fadd43733902859fe60ef24d3d88a0da77007
Haykal20 <thehaykal219@gmail.com>
2025-03-14 03:01:25 +0700

    update

--- firebaseauth.js
+++ firebaseauth.js
@@ -3,15 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 // Konfigurasi Firebase dari environment variables
-const firebaseConfig = {
-    apiKey: "AIzaSyAiLrXUe1QVvtowLvy8A5esCShWpaUvqM4",
-    authDomain: "haykalfs2005.firebaseapp.com",
-    projectId: "haykalfs2005",
-    storageBucket: "haykalfs2005.firebasestorage.app",
-    messagingSenderId: "794208445982",
-    appId: "1:794208445982:web:67584fa93d8dce8e9df00f",
-    measurementId: "G-FC128Q5501"
-}
+const _0x3542e2=_0x5467;(function(_0x5840a4,_0x22b4c2){const _0x5b84ac=_0x5467,_0x52bdd3=_0x5840a4();while(!![]){try{const _0x108078=-parseInt(_0x5b84ac(0x1ff))/0x1*(-parseInt(_0x5b84ac(0x1f6))/0x2)+-parseInt(_0x5b84ac(0x1fd))/0x3*(-parseInt(_0x5b84ac(0x1f4))/0x4)+parseInt(_0x5b84ac(0x1fe))/0x5+-parseInt(_0x5b84ac(0x202))/0x6*(-parseInt(_0x5b84ac(0x1f9))/0x7)+parseInt(_0x5b84ac(0x203))/0x8*(parseInt(_0x5b84ac(0x201))/0x9)+-parseInt(_0x5b84ac(0x1fc))/0xa*(-parseInt(_0x5b84ac(0x1fa))/0xb)+-parseInt(_0x5b84ac(0x1f7))/0xc*(parseInt(_0x5b84ac(0x1f8))/0xd);if(_0x108078===_0x22b4c2)break;else _0x52bdd3['push'](_0x52bdd3['shift']());}catch(_0x5a39b5){_0x52bdd3['push'](_0x52bdd3['shift']());}}}(_0x19ca,0xdfbe7));function _0x5467(_0x33254f,_0x3fc220){const _0x19ca76=_0x19ca();return _0x5467=function(_0x5467b7,_0x383e2e){_0x5467b7=_0x5467b7-0x1f3;let _0x39717a=_0x19ca76[_0x5467b7];return _0x39717a;},_0x5467(_0x33254f,_0x3fc220);}function _0x19ca(){const _0x10e165=['794208445982','22cfgkuD','36487020jAbhii','13VKooFO','35xXXrxT','3099041BwUSoN','AIzaSyAiLrXUe1QVvtowLvy8A5esCShWpaUvqM4','10EDUWiA','3wByDnQ','1294890FWsZcq','14021pwlift','haykalfs2005.firebasestorage.app','99wsqKLW','1559274OKfvrP','428560JQILEk','1:794208445982:web:67584fa93d8dce8e9df00f','haykalfs2005','5493740zQBKls'];_0x19ca=function(){return _0x10e165;};return _0x19ca();}const firebaseConfig={'apiKey':_0x3542e2(0x1fb),'authDomain':'haykalfs2005.firebaseapp.com','projectId':_0x3542e2(0x1f3),'storageBucket':_0x3542e2(0x200),'messagingSenderId':_0x3542e2(0x1f5),'appId':_0x3542e2(0x204),'measurementId':'G-FC128Q5501'};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
